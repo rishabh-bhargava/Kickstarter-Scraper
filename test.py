b25359799ab907c4fbd7592ce18946cd1f995f99
Rishabh Bhargava <rishabh.bhargava93@gmail.com>
2014-03-31 02:47:42 -0400

    Gets categories

--- test.py
+++ test.py
@@ -9,4 +9,30 @@ titles = [title.find("h2") for title in soup.findAll("div", "project-card")]
 links = [BASE_URL[0:len(BASE_URL)-1] + h2.a["href"] for h2 in titles]
 #h2 = title.find("h2"), BASE_URL[0:len(BASE_URL)-1] + 
 #link = BASE_URL[0:len(BASE_URL)-1] + h2.a["href"]
-print links
+
+#print links
+
+def get_categories():
+	html = urlopen(BASE_URL).read()
+	soup = BeautifulSoup(html, "lxml")
+	categ_list = soup.find("ul", "nav small_type")
+	categories = [li.text.encode('ascii', 'ignore').strip() for li in categ_list.findAll("li")]
+	print categories
+	return categories
+
+def main():
+	valid_categories = get_categories()
+	while True:
+		category = raw_input("Write category" + '\n')
+		if category in valid_categories:
+			break
+		print "Invalid category; try again."
+
+
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print '\nGoodbye!'
